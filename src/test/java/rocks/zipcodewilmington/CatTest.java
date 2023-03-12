c3c1b8f9b46e9b10ed72ee4aeae94c7b545f4028
ule270 <u.le270@gmail.com>
2023-03-12 15:41:19 -0400

    cat tests.

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -2,7 +2,10 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
 
 import java.util.Date;
 
@@ -10,16 +13,120 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class CatTest {
-    // TODO - Create tests for `void setName(String name)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
-
 
     @Test
+    public void testNewCat() {
+//        given
+        String name = "Kiki";
+        Cat cat = new Cat(name, new Date(), 2);
+
+//        when
+        String expected = "Kitty";
+        cat.setName(expected);
+        String actual = cat.getName();
+
+//        then
+        Assert.assertEquals(expected, actual);
+    }
+    // TODO - Create tests for `void setName(String name)`
+    @Test
+    public void setName() {
+        //    given
+        Cat cat = new Cat("Kiki", new Date(), 2);
+        String givenName = "Kiki";
+        //        when
+        cat.setName(givenName);
+//        then
+        String catName = cat.getName();
+        Assert.assertEquals(catName, givenName);
+    }
+
+    // TODO - Create tests for `speak`
+
+    @Test
+    public void testSpeak() {
+        //    given
+        Cat cat = new Cat("Kiki", new Date(), 2);
+        String expected = "meow!";
+        //        when
+        String actual = cat.speak();
+//        then
+        Assert.assertEquals(expected, actual);
+    }
+
+    // TODO - Create tests for `setBirthDate(Date birthDate)`
+
+    @Test
+    public void testBirthDate() {
+//    given
+        Cat cat = new Cat("Kiki", new Date(), 2);
+        Date expected = new Date(2023, 3, 17);
+        //        when
+        cat.setBirthDate(expected);
+        Date actual = cat.getBirthDate();
+
+//        then
+        Assert.assertEquals(expected, actual);
+    }
+
+    // TODO - Create tests for `void eat(Food food)`
+
+    @Test
+    public void testEatFood() {
+    //    given
+    Cat cat = new Cat("Kiki", new Date(), 2);
+    Integer expected = 1;
+    Food food = new Food();
+    //        when
+        cat.eat(food);
+    Integer actual = cat.getNumberOfMealsEaten();
+
+//        then
+        Assert.assertEquals(expected, actual);
+}
+
+    // TODO - Create tests for `Integer getId()`
+    @Test
+    public void testId() {
+//        given
+        Cat cat = new Cat("Kiki", new Date(), 2);
+        Integer expected = 2;
+
+//        when
+        Integer actual = cat.getId();
+
+//        then
+        Assert.assertEquals(expected, actual);
+    }
+
+    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+
+    @Test
+    public void testAnimalInheritance() {
+//        given
+        Cat cat = new Cat("Kiki", new Date(), 2);
+        boolean expected = true;
+
+//        when
+        boolean actual = cat instanceof Animal;
+
+//        then
+        Assert.assertEquals(expected, actual);
+    }
+
+    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    @Test
+    public void testMammalInheritance(){
+//        given
+        Cat cat = new Cat("Kiki", new Date(), 2);
+        boolean expected = true;
+
+//        when
+        boolean actual = cat instanceof Mammal;
+
+//        then
+        Assert.assertEquals(expected, actual);
+    }
     public void constructorTest() {
         // Given (cat data)
         String givenName = "Zula";
